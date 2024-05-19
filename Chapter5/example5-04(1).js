4c5c31d73f702459ee976e67fe622e17b59d899e
dwchoii <cdw4777@naver.com>
2024-05-19 15:06:17 +0900

    Closer occurs without return

--- Chapter5/example5-04(1).js
+++ Chapter5/example5-04(1).js
@@ -1,13 +1,13 @@
 // (1) setInterval/setTimeout
 (function() {
-    var a = 0;
-    var intervalId = null;
-    var inner = function() {
-      if (++a >= 10) {
-        clearInterval(intervalId);
-      }
-      console.log(a);
-    };
-    intervalId = setInterval(inner, 1000);
-  })();
+  var a = 0;
+  var intervalId = null;
+  var inner = function() {
+    if (++a >= 10) {
+      clearInterval(intervalId);
+    }
+    console.log(a);
+  };
+  intervalId = setInterval(inner, 1000);
+})();
 
